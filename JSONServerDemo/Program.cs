5c7e44508d1f386271a0a46ce4a04ef09b44fe97
r-svp26 <riteshk6341@gmail.com>
2021-09-08 16:44:21 +0530

    [Ritesh] Add: Rest API testing using Postman

---
+++ JSONServerDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace JSONServerDemo
+{
+    class Program
+    {
+        /// <summary>
+        /// main method
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to JSON Server");
+        }
+    }
+}
